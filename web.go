61a419c694e6aec53ae4bccc962d2f92e800e2d2
dbubel <dbubel@bishopfox.com>
2019-10-11 10:36:11 -0400

    cleanups and upgrade httprouter

--- web.go
+++ web.go
@@ -42,18 +42,6 @@ func (a *App) SetLoggingLevel(level log.Level) {
 	l.SetLevel(level)
 }
 
-//func (a *App) allowMethod(method string) func(next http.Handler) http.Handler {
-//	return func(next http.Handler) http.Handler {
-//		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//			if r.Method == method {
-//				next.ServeHTTP(w, r)
-//			} else {
-//				Respond(w, r, map[string]interface{}{"error": "Method Now Allowed"}, http.StatusMethodNotAllowed)
-//			}
-//		})
-//	}
-//}
-
 func (a *App) Handle(verb string, path string, finalHandler Handler, middlwares ...MiddleWare) {
 
 	// Our handler that we will pass to the router
@@ -65,25 +53,6 @@ func (a *App) Handle(verb string, path string, finalHandler Handler, middlwares 
 			h = middlwares[i](h)
 		}
 	}
-
-	// Assign a middleware that will only allow the specified verb
-	//if verb == http.MethodGet {
-	//	a.globalMiddleware = append(a.globalMiddleware, a.allowMethod(http.MethodGet))
-	//} else if verb == http.MethodOptions {
-	//	a.globalMiddleware = append(a.globalMiddleware, a.allowMethod(http.MethodOptions))
-	//} else if verb == http.MethodDelete {
-	//	a.globalMiddleware = append(a.globalMiddleware, a.allowMethod(http.MethodDelete))
-	//} else if verb == http.MethodPost {
-	//	a.globalMiddleware = append(a.globalMiddleware, a.allowMethod(http.MethodPost))
-	//} else if verb == http.MethodPut {
-	//	a.globalMiddleware = append(a.globalMiddleware, a.allowMethod(http.MethodPut))
-	//} else if verb == http.MethodConnect {
-	//	a.globalMiddleware = append(a.globalMiddleware, a.allowMethod(http.MethodConnect))
-	//} else if verb == http.MethodHead {
-	//	a.globalMiddleware = append(a.globalMiddleware, a.allowMethod(http.MethodHead))
-	//} else if verb == http.MethodTrace {
-	//	a.globalMiddleware = append(a.globalMiddleware, a.allowMethod(http.MethodTrace))
-	//}
 
 	builtinMiddlwares := []MiddleWare{
 		// Add a start timer middleware to the beginning of global middlware slice
@@ -104,40 +73,6 @@ func (a *App) Handle(verb string, path string, finalHandler Handler, middlwares 
 		}
 	}
 
-	//a.debug(rr{"path": path, "msg": "added route"})
 	l.WithFields(log.Fields{"path":path}).Info("added route")
-	//a.Router.Handle(path, h)
 	a.Router.Handler(verb,path,h)
-	//a.Router.HandleFunc()
-
-	//finalHandler := http.HandlerFunc(final)
-	//x := middlewareOne(middlewareTwo(finalHandler))
-	//http.Handle("/", h)
-	// Wrap up the application-wide first, this will call the first function
-	// of each middleware which will return a function of type Handler.``
-	//finalHandler = wrapMiddleware(finalHandler, middlwares)
-	//finalHandler = wrapMiddleware(finalHandler, a.middlwares)
-
-	// The function to execute for each request.
-	//h := func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	// Setup CORS for development - Dont do this unless its a public endpoint obviously
-	// w.Header().Set("Access-Control-Allow-Origin", "*")
-	// w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	// Call the wrapped finalHandler functions.
-	//a.Router.ServeHTTP(w, r)
-	//finalHandler(a.log, w, r)
-	//}
-	// Add this finalHandler for the specified verb and route.
-	//a.Router.Handle(verb, path, h)
-	//a.Router.HandleFunc(path, h)
-	//a.Handle(verb, path, h)
-
-	//Handle preflight browser requests when we are POSTing data
-	// f := func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	// 	// Setup CORS for dev only or public endpoint
-	// 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	// 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	// 	return
-	// }
-	// a.Router.Handle("OPTIONS", path, f)
 }
--- web_test.go
+++ web_test.go
@@ -31,48 +31,41 @@ func TestRouteSimple(t *testing.T) {
 	assert.JSONEq(t,`{"msg":"payload"}`,string(json) )
 }
 
-//
-//func TestMiddleware(t *testing.T) {
-//	mux := httprouter.New()
-//
-//	globalMiddle := func(next http.Handler) http.Handler {
-//		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//			log.Println("Executing globalmiddle")
-//			next.ServeHTTP(w, r)
-//		})
-//	}
-//
-//	var app = New(mux, globalMiddle)
-//
-//	testHandler := func(w http.ResponseWriter, r *http.Request) {
-//		log.Println("Executing handler")
-//		Respond(w, r, "hi", http.StatusOK)
-//	}
-//
-//	testMiddlware := func(next http.Handler) http.Handler {
-//		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//			log.Println("Executing middlewareOne")
-//			next.ServeHTTP(w, r)
-//		})
-//	}
-//
-//	testMiddlware2 := func(next http.Handler) http.Handler {
-//		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//			log.Println("Executing middlewareTwo")
-//			next.ServeHTTP(w, r)
-//		})
-//	}
-//
-//	app.Handle(http.MethodGet, "/test", testHandler, testMiddlware, testMiddlware2)
-//
-//	r := httptest.NewRequest(http.MethodGet, "/test", nil)
-//	w := httptest.NewRecorder()
-//
-//	app.Router.ServeHTTP(w, r)
-//	assert.Equal(t, http.StatusOK, w.Code, "Response code should be 200")
-//	b, _ := ioutil.ReadAll(w.Body)
-//
-//	fmt.Println("BODYd", string(b))
-//
-//}
+func TestMiddleware(t *testing.T) {
+	mux := httprouter.New()
+
+	globalMiddle := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	var app = New(mux, globalMiddle)
+
+	testHandler := func(w http.ResponseWriter, r *http.Request) {
+		Respond(w, r, "hi", http.StatusOK)
+	}
+
+	testMiddlware := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	testMiddlware2 := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	app.Handle(http.MethodGet, "/test", testHandler, testMiddlware, testMiddlware2)
+
+	r := httptest.NewRequest(http.MethodGet, "/test", nil)
+	w := httptest.NewRecorder()
+
+	app.Router.ServeHTTP(w, r)
+	assert.Equal(t, http.StatusOK, w.Code, "Response code should be 200")
+	_, _ := ioutil.ReadAll(w.Body)
+
+}
 
